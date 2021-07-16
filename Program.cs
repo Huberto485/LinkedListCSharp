5fc77ce6b4b50ca585cffd138acf000e783ff26f
Huberto485 <thehubert.antczak@gmail.com>
2021-07-16 20:21:31 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace LinkedListExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This application uses Linked Lists collection to demonstrate my capabilities :)");
+            Console.WriteLine();
+
+            string[] words = { "age", "is", "just", "an", "illusion", "of", "time" };
+
+            LinkedList<string> sentence = new LinkedList<string>(words);
+            
+            //Display entire list
+            Display(sentence, "[Test 1] - Display all words:: ");
+
+        }
+
+        private static void Display(LinkedList<string> words, string test)
+        {
+            Console.Write(test);
+
+            foreach (string word in words)
+            {
+                Console.Write(word + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Reverse(LinkedList<string> words, string test)
+        {
+            Console.Write(test);
+
+
+        }
+    }
+}
